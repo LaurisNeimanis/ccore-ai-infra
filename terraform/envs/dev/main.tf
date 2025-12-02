51bc1fe0df60722f01f22d4c373485786a546377
Lauris Neimanis <neimanis.lauris@gmail.com>
2025-12-02 21:29:57 +0200

    Enable local provider and include Terraform lock file for reproducible builds

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -2,12 +2,17 @@
 # TERRAFORM SETTINGS
 # --------------------------------------------
 terraform {
-  required_version = "~> 1.14.0"
+  required_version = "~> 1.14"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
     }
   }
 }
